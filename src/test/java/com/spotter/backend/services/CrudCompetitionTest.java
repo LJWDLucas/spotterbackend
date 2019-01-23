83af64071207d4924d27fcf808abc02b62fa5844
Danny Lucas <danny.lucas@jstack.eu>
2019-01-23 20:01:48 +0100

    no message

--- src/test/java/com/spotter/backend/services/CrudCompetitionTest.java
+++ src/test/java/com/spotter/backend/services/CrudCompetitionTest.java
@@ -1,12 +1,44 @@
 package com.spotter.backend.services;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+import org.mockito.Mock;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CrudCompetitionTest {
+
+    @Mock
+    private ResultSet rs;
+
+    @Mock
+    private DatabaseService db;
+
+    @Mock
+    private PreparedStatement stmt;
+
+    @Mock
+    private Connection c;
+
     private Crud crud = new CrudCompetition();
+
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Mockito.when(c.prepareStatement(Mockito.any(String.class))).thenReturn(stmt);
+        Mockito.when(db.getConnection()).thenReturn(c);
+    }
 
     @Test
     public void testGetCompetition(){
 
     }
+
 }
